0799d59bfee881958bc7af1c74474f0995d7ccbf
simon3000 <simon3000@163.com>
2019-06-26 21:21:37 +0800

    vue create

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+  ],
+  rules: {
+    'space-before-function-paren': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
