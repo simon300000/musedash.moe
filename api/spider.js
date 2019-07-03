9bf9979e43dd823af65ad46f0456bc1f0081f185
simon3000 <simon3000@163.com>
2019-07-03 19:14:32 +0200

    download and save rank

--- api/spider.js
+++ api/spider.js
@@ -1,3 +1,5 @@
+const got = require('got')
+
 const INTERVAL = 1000 * 60 * 60 * 24
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
@@ -7,6 +9,8 @@ const platforms = {
   pc: 'pcleaderboard'
 }
 
+const download = async ({ api, uid, difficulty }) => (await got(`https://prpr-muse-dash.leanapp.cn/musedash/v1/${api}/top?music_uid=${uid}&music_difficulty=${difficulty + 1}&limit=1999`, { json: true })).body.result
+
 const prepare = music => music
   .flatMap(({ uid, difficulty, name }) => difficulty
     .map((difficultyNum, difficulty) => ({ uid, level: difficultyNum, difficulty, name })))
@@ -14,11 +18,28 @@ const prepare = music => music
     .map(([platform, api]) => ({ uid, difficulty, name, level, platform, api })))
   .filter(({ level }) => level)
 
+const round = async ({ musicList, rank }) => {
+  let pending = [...musicList]
+  for (; pending.length;) {
+    const { uid, difficulty, name, platform, api } = pending.shift()
+    let result = await download({ uid, difficulty, api }).catch(() => undefined)
+    if (!result) {
+      pending.push({ uid, difficulty, name, platform, api })
+      console.log(`RETRY: ${uid}: ${name} - ${difficulty} - ${platform}`)
+      continue
+    }
+
+    await rank.put({ uid, difficulty, platform, value: result })
+
+    console.log(`${uid}: ${name} - ${difficulty} - ${platform}`)
+  }
+}
+
 module.exports = async ({ music, rank }) => {
   for (;;) {
     let startTime = Date.now()
-    let pending = prepare(music)
-    console.log(pending)
+    let musicList = prepare(music)
+    await round({ musicList, rank })
     let endTime = Date.now()
     console.log(`Wait ${INTERVAL - (endTime - startTime)}`)
     await wait(INTERVAL - (endTime - startTime))
