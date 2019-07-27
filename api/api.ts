d7d56dcce308815a8ba8bab0cc4be091f548c29b
simon3000 <simon3000@163.com>
2019-07-27 02:55:17 +0200

    koa http cache

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,23 @@
 import Koa = require('koa')
 import Router = require('koa-router')
+import LRU = require('lru-cache')
+
+const cache = new LRU({
+  maxAge: 1000 * 5,
+  max: 100
+})
 
 const app = new Koa()
+
+app.use(async (ctx, next) => {
+  let hit = cache.get(ctx.url)
+  if (hit) {
+    ctx.body = hit
+  } else {
+    await next()
+    cache.set(ctx.url, ctx.body)
+  }
+})
 
 export default ({ albums }) => {
   const router = new Router()
