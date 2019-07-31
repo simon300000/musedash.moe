904622a4eee1843e8ef32fa208413a19fb73fa5c
simon3000 <simon3000@163.com>
2019-07-31 07:44:26 +0200

    state injection

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,6 +1,10 @@
 import { createApp } from './app'
 import './registerServiceWorker'
 
-const { app } = createApp()
+const { app, store } = createApp()
+
+if (window.__INITIAL_STATE__) {
+  store.replaceState(window.__INITIAL_STATE__)
+}
 
 app.$mount('#app')
--- src/entry-server.js
+++ src/entry-server.js
@@ -2,7 +2,7 @@ import { createApp } from './app'
 
 export default context => {
   return new Promise((resolve, reject) => {
-    const { app, router } = createApp()
+    const { app, router, store } = createApp()
 
     router.push(context.url)
 
@@ -11,6 +11,9 @@ export default context => {
       if (!matchedComponents.length) {
         // eslint-disable-next-line prefer-promise-reject-errors
         return reject({ code: 404 })
+      }
+      context.rendered = () => {
+        context.state = store.state
       }
 
       resolve(app)
