40e0ddb90c76f80f3c49942ca8780e4356dfc46a
simon3000 <simon3000@163.com>
2019-08-01 14:53:25 +0200

    vue-analytics

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,10 +1,20 @@
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+
 import { createApp } from './app'
 import './registerServiceWorker'
 
-const { app, store } = createApp()
+const { app, store, router } = createApp()
 
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
+}
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-123973162-5',
+    router
+  })
 }
 
 app.$mount('#app')
