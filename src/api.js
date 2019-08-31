5e85e7989916ab99c2625ee916a7490a085c5708
simon3000 <simon3000@163.com>
2019-09-01 00:22:38 +0200

    dev env api proxy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const url = process.env.NODE_ENV === 'development' ? 'https://api.musedash.moe/' : new Vue().$isServer ? 'http://0.0.0.0:8301/' : 'https://api.musedash.moe/'
+const url = process.env.NODE_ENV === 'development' ? '/api/' : new Vue().$isServer ? 'http://0.0.0.0:8301/' : 'https://api.musedash.moe/'
 
 const get = async api => (await axios(`${url}${api}`)).data
 
--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,16 @@ module.exports = {
       entry: 'src/entry-client.js',
       template: 'index.html'
     }
+  },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://api.musedash.moe',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/': '/'
+        }
+      }
+    }
   }
 }
