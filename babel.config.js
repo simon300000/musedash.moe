2c869a259f9f92e461b3e725b44edf9b855ff346
simon3000 <simon30002021@icloud.com>
2019-10-29 16:59:17 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = api => {
   api.cache(true)
 
-  const presets = ['@vue/app']
+  const presets = ['@vue/cli-plugin-babel/preset']
   const plugins = ['@babel/plugin-proposal-optional-chaining', '@babel/plugin-transform-runtime']
 
   return {
