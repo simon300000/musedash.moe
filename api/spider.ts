177b17fc416f11a8e2dba0e329830f7efb6033f6
simon3000 <simon30002021@icloud.com>
2019-11-21 04:04:08 +0100

    hide some log

--- api/spider.ts
+++ api/spider.ts
@@ -17,7 +17,7 @@ const downloadCore = async ({ api, uid, difficulty }): Promise<APIResults | void
 const download = async ({ api, uid, difficulty }): Promise<APIResults> => {
   const result = await downloadCore({ api, uid, difficulty })
   if (!result) {
-    console.log('retry')
+    // console.log('retry')
     await wait(1000 * 60 * Math.random())
     return download({ api, uid, difficulty })
   } else {
@@ -40,7 +40,7 @@ const core = async ({ pending, rank }: { pending: ReturnType<typeof prepare>, ra
       .filter(({ play, user }) => play && user)
     if (!result.length) {
       pending.unshift(music)
-      console.log(`RETRY: ${uid}: ${name} - ${difficulty} - ${platform}`)
+      // console.log(`RETRY: ${uid}: ${name} - ${difficulty} - ${platform}`)
       continue
     }
 
@@ -56,7 +56,7 @@ const core = async ({ pending, rank }: { pending: ReturnType<typeof prepare>, ra
 
     await rank.put({ uid, difficulty, platform, value: resultWithHistory })
 
-    console.log(`${uid}: ${name} - ${difficulty} - ${platform} / ${pending.length}`)
+    // console.log(`${uid}: ${name} - ${difficulty} - ${platform} / ${pending.length}`)
   }
 }
 
