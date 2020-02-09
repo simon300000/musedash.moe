2a16339283457aa75c06b6d84a803362254abf69
simon3000 <simon30002021@icloud.com>
2020-02-09 22:13:29 +0100

    max_old_space_size 8192

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,15 @@
+import { setFlagsFromString } from 'v8'
+
 import parser from './albumParser'
 
 import { rank, player, search } from './database'
 import spider from './spider'
 
 import api from './api'
+
+setFlagsFromString('--max_old_space_size=8192')
+
+;
 
 (async () => {
   const albums = await parser()
