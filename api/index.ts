b3bd45ad86a2fcd4a54eb0485d8d80ba584794fa
simon3000 <simon30002021@icloud.com>
2020-02-09 23:03:50 +0100

    Revert "max_old_space_size 8192"
    This reverts commit 2a16339283457aa75c06b6d84a803362254abf69.

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,9 @@
-import { setFlagsFromString } from 'v8'
-
 import parser from './albumParser'
 
 import { rank, player, search } from './database'
 import spider from './spider'
 
 import api from './api'
-
-setFlagsFromString('--max_old_space_size=8192')
-
-;
 
 (async () => {
   const albums = await parser()
