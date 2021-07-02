1b97f33f9a9543665cef1f3d96351024380516a6
simon3000 <simon30002021@icloud.com>
2021-07-03 07:57:24 +0800

    Update vue.config.cjs

--- vue.config.cjs
+++ vue.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        // target: 'https://api.musedash.moe', // use production api
-        target: 'http://0.0.0.0:8301', // local api
+        target: 'https://api.musedash.moe', // use production api
+        // target: 'http://0.0.0.0:8301', // local api
         changeOrigin: true,
         pathRewrite: {
           '^/api/': '/'
