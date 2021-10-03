d8e54cc13e1408f71e80f209b8bc2e337e8cbc73
simon3000 <simon30002021@icloud.com>
2021-10-04 07:42:10 +0800

    update merge

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,4 +1,4 @@
-import merge from 'webpack-merge'
+import { merge } from 'webpack-merge'
 import baseConfig from './webpack.base.config.js'
 import VueSSRClientPlugin from 'vue-server-renderer/client-plugin.js'
 
