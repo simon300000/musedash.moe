e368a54d5b764e559368851d7d9aaa6fb98e45a4
simon3000 <w@simon3k.moe>
2022-03-06 11:58:44 +0800

    Update api.ts

--- api/api.ts
+++ api/api.ts
@@ -27,9 +27,12 @@ export const app = new Koa()
 
 app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', 'https://musedash.moe')
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type')
   ctx.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-  await next()
+  if (ctx.method === 'OPTIONS') {
+    ctx.body = ''
+  } else {
+    await next()
+  }
 })
 
 app.use(async (ctx, next) => {
