fc5e119b15bf3ff47912751ea0734fdeb03fa421
simon3000 <simon30002021@icloud.com>
2023-02-25 10:54:20 +0100

    Update removelastrl.js

--- script/removelastrl.js
+++ script/removelastrl.js
@@ -6,6 +6,12 @@ const db = new RaveLevel('./db')
 const playerDiffHistory = db.sublevel('playerDiffHistory', { valueEncoding: 'json' })
 const playerDiffHistoryNumber = playerDiffHistory.sublevel('number', { valueEncoding: 'json' })
 
+let count = 0
+
 for await (const [key, value] of playerDiffHistoryNumber.iterator()) {
   await playerDiffHistoryNumber.put(key, value - 1)
+  count++
+  if (count % 1000 === 0) {
+    console.log(count)
+  }
 }
