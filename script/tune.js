a5bc6b223f7425e360aa6535a28005f5f63aac20
simon3000 <simon30002021@icloud.com>
2023-02-28 00:09:37 +0100

    tunes

---
+++ script/tune.js
@@ -0,0 +1,21 @@
+import { writeFile, readFile } from 'fs/promises'
+
+const toXYCSV = data => `x,y\n${data.map(({ x, y }) => `${x},${y}`).join('\n')}`
+
+const loadPlayer = async type => JSON.parse(await readFile(`tune-${type}-playerdiff.json`))
+
+const basePlayer = await loadPlayer('base')
+const newPlayer = await loadPlayer('sort')
+
+const basePlayerRL = Object.fromEntries(basePlayer.map(({ id, rl }) => [id, rl]))
+const newPlayerRL = Object.fromEntries(newPlayer.map(({ id, rl }) => [id, rl]))
+
+const difference = basePlayer
+  .filter(({ rl }) => rl > 7)
+  .map(({ id, rl }) => ({ rl, diff: newPlayerRL[id] - rl })).map(({ rl, diff }) => ({ x: rl, y: diff }))
+
+await writeFile('tune-sort-playerdiff.csv', toXYCSV(difference))
+
+const averageDifference = difference.reduce((sum, { y }) => sum + y, 0) / difference.length
+
+console.log(averageDifference)
