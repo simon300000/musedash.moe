1bf7a6375652006428120097c08f4399a99f4a2b
simon3000 <simon30002021@icloud.com>
2023-02-28 02:16:37 +0100

    36p csv

--- script/tune.js
+++ script/tune.js
@@ -5,7 +5,7 @@ const toXYCSV = data => `x,y\n${data.map(({ x, y }) => `${x},${y}`).join('\n')}`
 const loadPlayer = async type => JSON.parse(await readFile(`tune-${type}-playerdiff.json`))
 
 const basePlayer = await loadPlayer('base')
-const newPlayer = await loadPlayer('sort')
+const newPlayer = await loadPlayer('36p')
 
 const basePlayerRL = Object.fromEntries(basePlayer.map(({ id, rl }) => [id, rl]))
 const newPlayerRL = Object.fromEntries(newPlayer.map(({ id, rl }) => [id, rl]))
@@ -14,7 +14,7 @@ const difference = basePlayer
   .filter(({ rl }) => rl > 8)
   .map(({ id, rl }) => ({ rl, diff: newPlayerRL[id] - rl })).map(({ rl, diff }) => ({ x: rl, y: diff }))
 
-await writeFile('tune-sort-playerdiff.csv', toXYCSV(difference))
+await writeFile('tune-36p-playerdiff.csv', toXYCSV(difference))
 
 const averageDifference = difference.reduce((sum, { y }) => sum + y, 0) / difference.length
 
