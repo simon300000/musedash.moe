eb08af904b2e3a79007fe89db297511f3c2a2118
simon3000 <simon30002021@icloud.com>
2023-02-28 06:20:47 +0100

    36pa

--- script/tune.js
+++ script/tune.js
@@ -5,7 +5,7 @@ const toXYCSV = data => `x,y\n${data.map(({ x, y }) => `${x},${y}`).join('\n')}`
 const loadPlayer = async type => JSON.parse(await readFile(`tune-${type}-playerdiff.json`))
 
 const basePlayer = await loadPlayer('base')
-const newPlayer = await loadPlayer('36p')
+const newPlayer = await loadPlayer('36pa')
 
 const basePlayerRL = Object.fromEntries(basePlayer.map(({ id, rl }) => [id, rl]))
 const newPlayerRL = Object.fromEntries(newPlayer.map(({ id, rl }) => [id, rl]))
@@ -14,8 +14,12 @@ const difference = basePlayer
   .filter(({ rl }) => rl > 8)
   .map(({ id, rl }) => ({ rl, diff: newPlayerRL[id] - rl })).map(({ rl, diff }) => ({ x: rl, y: diff }))
 
-await writeFile('tune-36p-playerdiff.csv', toXYCSV(difference))
+await writeFile('tune-36pa-playerdiff.csv', toXYCSV(difference))
 
-const averageDifference = difference.reduce((sum, { y }) => sum + y, 0) / difference.length
-
-console.log(averageDifference)
+for (let index = 0; index < 13; index++) {
+  const d = basePlayer
+    .filter(({ rl }) => rl > index && rl < index + 1)
+    .map(({ id, rl }) => ({ rl, diff: newPlayerRL[id] - rl })).map(({ rl, diff }) => ({ x: rl, y: diff }))
+  const averageDifference = d.reduce((sum, { y }) => sum + y, 0) / d.length
+  console.log(averageDifference)
+}
