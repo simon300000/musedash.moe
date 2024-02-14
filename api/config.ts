bc0485c3d92b464a7860d848f324de19e21fff02
Simon <simon30002021@icloud.com>
2024-02-14 21:12:24 +0100

    Update config.ts

--- api/config.ts
+++ api/config.ts
@@ -1,4 +1,4 @@
 export const SPIDER_PARALLEL = 256
 
-export const characterSkip = ['1', '2', '12', '13', '14','22']
+export const characterSkip = ['1', '2', '12', '13', '14', '19', '22', '25', '26']
 export const elfinSkip = ['3', '8', '10']
