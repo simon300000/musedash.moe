5c76556c723a76a7ae7020dcc140b3470d30d623
Simon <simon30002021@icloud.com>
2025-03-04 20:34:06 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@
         <router-link to="/player" class="navbar-item" :exact-active-class="'is-active'">Search</router-link>
         <router-link to="/mdmc" class="navbar-item mdmcPink" :active-class="'is-active'">mdmc</router-link>
         <router-link to="/rd" class="navbar-item" :active-class="'is-active'">R. Difficulty</router-link>
+        <p class="navbar-item">Sorry for the late update!</p>
       </div>
 
       <div class="navbar-end">
