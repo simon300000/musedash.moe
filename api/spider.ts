2184355a8821c3a3f7c3c2bd5daeeec1685568e6
Simon <simon30002021@icloud.com>
2025-11-18 16:15:24 +0800

    Adjust nextDownload timing on failed spider run

--- api/spider.ts
+++ api/spider.ts
@@ -369,7 +369,7 @@ const spider = async () => {
       if (success) {
         next.nextDownload = Date.now() + 1000 * 60 * 60 * 20
       } else {
-        next.nextDownload = Date.now() - 1000 * 60
+        next.nextDownload = Date.now() + 1000 * 60 * 60 * 2
       }
       next.ready = true
       if (next.pending) {
